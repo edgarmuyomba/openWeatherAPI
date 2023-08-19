201ce50f8ebbd5bf1267a0c949f322b199c87baa
edgarmuyomba <edgarmatthew247@gmail.com>
2023-08-19 23:18:08 +0300

    startapp

---
+++ data/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import * 
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass 
+
+@admin.register(Weather)
+class WeatherAdmin(admin.ModelAdmin):
+    pass
---
+++ data/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    lat = models.DecimalField(max_digits=5, decimal_places=2)
+    lon = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Weather(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    dt = models.DateTimeField()
+    condition = models.CharField(max_length=50)
+    temp = models.IntegerField()
+    pressure = models.IntegerField()
+    humidity = models.IntegerField()
